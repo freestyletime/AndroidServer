eace068bb858fbdfbdf54c4f384e5b7381151628
Christian Chen <mailchristianchen@gmail.com>
2014-10-13 14:55:41 +0800

    gen remove

--- Server/gen/cn/christian/server/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package cn.christian.server;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
